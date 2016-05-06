ff9dfec09928d586e7a15f1556e1b6126d5f6475
Ray <savageno2@hotmail.com>
2016-05-06 15:37:07 +0800

    更改项目名称

---
+++ chingluh_android_app/src/main/java/com/chingluh/android/activity/withoutlogin/LocalMenuActivity.java
@@ -0,0 +1,18 @@
+package com.chingluh.android.activity.withoutlogin;
+
+import android.os.Bundle;
+
+import com.chingluh.android.R;
+import com.chingluh.android.base.BaseActivityWithoutLogin;
+
+/**
+ * Created by Ray on 2016/05/06.
+ */
+public class LocalMenuActivity extends BaseActivityWithoutLogin{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_local_menu);
+	}
+}
