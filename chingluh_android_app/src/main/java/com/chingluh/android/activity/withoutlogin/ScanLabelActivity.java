0e04bf44b6637ac8f9c70f6189e7dabb235f3c96
Ray <savageno2@hotmail.com>
2016-05-09 11:33:18 +0800

    项目调整

--- chingluh_android_app/src/main/java/com/chingluh/android/activity/withoutlogin/ScanLabelActivity.java
+++ chingluh_android_app/src/main/java/com/chingluh/android/activity/withoutlogin/ScanLabelActivity.java
@@ -1,36 +1,25 @@
 package com.chingluh.android.activity.withoutlogin;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TabHost;
+import android.view.View;
+import android.widget.Button;
 
 import com.chingluh.android.R;
 import com.chingluh.android.base.BaseActivityWithoutLogin;
 
 /**
- * Created by Ray on 2016/05/09.
+ * Created by Ray on 2016/05/06.
  */
 public class ScanLabelActivity extends BaseActivityWithoutLogin{
-	private TabHost tabHostScanlabel;
+	public View.OnClickListener onClickListener=new View.OnClickListener(){
+		@Override
+		public void onClick(View v){
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_scanlabel);
-		//
-		tabHostScanlabel=(TabHost)findViewById(R.id.tabHostScanlabel);
-		Intent intentSet=new Intent(this,ScanLabelSetActivity.class);
-		Intent intent6B=new Intent(this,ScanLabel6BActivity.class);
-		Intent intent6C=new Intent(this,ScanLabel6CActivity.class);
-		//
-		TabHost.TabSpec tabSpecSet=tabHostScanlabel.newTabSpec(getString(R.string.TabHost_Set_Label)).setIndicator(getString(R.string.TabHost_Set_Label)).setContent(intentSet);
-		TabHost.TabSpec tabSpec6B=tabHostScanlabel.newTabSpec(getString(R.string.TabHost_6B_Label)).setIndicator(getString(R.string.TabHost_6B_Label)).setContent(intent6B);
-		TabHost.TabSpec tabSpec6C=tabHostScanlabel.newTabSpec(getString(R.string.TabHost_6C_Label)).setIndicator(getString(R.string.TabHost_6C_Label)).setContent(intent6C);
-		//
-		tabHostScanlabel.addTab(tabSpecSet);
-		tabHostScanlabel.addTab(tabSpec6B);
-		tabHostScanlabel.addTab(tabSpec6C);
-		//
-		tabHostScanlabel.setCurrentTab(0);
+		setContentView(R.layout.activity_scanlabel_set);
 	}
 }
