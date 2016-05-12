0c3c9d92d61e705441f280897d0676868a3268d8
savageno1 <savageno2@hotmail.com>
2016-05-12 23:08:41 +0800

    增加FtpUtil

--- chingluh_android_app/src/main/java/com/chingluh/android/util/FtpUtil.java
+++ chingluh_android_app/src/main/java/com/chingluh/android/util/FtpUtil.java
@@ -1,6 +1,10 @@
 package com.chingluh.android.util;
 
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+
+import java.io.IOException;
+import java.net.SocketException;
 
 /**
  * Created by Ray on 2016/05/06.
@@ -8,6 +12,10 @@ import org.apache.commons.net.ftp.FTPClient;
 public class FtpUtil{
 	private static FtpUtil ftpUtilInstance=null;
 	private FTPClient ftpClient;
+	private String url;
+	private int port;
+	private String userName;
+	private String userPassword;
 
 	private FtpUtil(){
 		ftpClient=new FTPClient();
@@ -18,5 +26,28 @@ public class FtpUtil{
 			ftpUtilInstance=new FtpUtil();
 		}
 		return ftpUtilInstance;
+	}
+	public boolean initFTPSetting(String url,int port,String userName,String userPassword){
+		this.url=url;
+		this.port=port;
+		this.userName=userName;
+		this.userPassword=userPassword;
+		int reply;
+		try{
+			ftpClient.connect(url,port);
+			ftpClient.login(userName,userPassword);
+			reply=ftpClient.getReplyCode();
+			if(!(FTPReply.isPositiveCompletion(reply))){
+				ftpClient.disconnect();
+				return  false;
+			}
+			return  true;
+		}catch(SocketException exception){
+			exception.printStackTrace();
+			return true;
+		}catch(IOException exception){
+			exception.printStackTrace();
+			return  true;
+		}
 	}
 }
