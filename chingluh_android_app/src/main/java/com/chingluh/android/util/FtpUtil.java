43a27fc186e4b49c697b50e5188ca7ab79b2aa9b
savageno1 <savageno2@hotmail.com>
2016-05-12 22:32:19 +0800

    增加FtpUtil

--- chingluh_android_app/src/main/java/com/chingluh/android/util/FtpUtil.java
+++ chingluh_android_app/src/main/java/com/chingluh/android/util/FtpUtil.java
@@ -1,7 +1,22 @@
 package com.chingluh.android.util;
 
+import org.apache.commons.net.ftp.FTPClient;
+
 /**
  * Created by Ray on 2016/05/06.
  */
 public class FtpUtil{
+	private static FtpUtil ftpUtilInstance=null;
+	private FTPClient ftpClient;
+
+	private FtpUtil(){
+		ftpClient=new FTPClient();
+	}
+
+	public static FtpUtil getInstance(){
+		if(ftpUtilInstance==null){
+			ftpUtilInstance=new FtpUtil();
+		}
+		return ftpUtilInstance;
+	}
 }
