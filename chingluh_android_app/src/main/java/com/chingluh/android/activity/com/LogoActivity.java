659458c48dc2a9aeb9038e9200157e448564a316
Ray <savageno2@hotmail.com>
2016-05-13 14:35:33 +0800

    Logo动画效果

--- chingluh_android_app/src/main/java/com/chingluh/android/activity/com/LogoActivity.java
+++ chingluh_android_app/src/main/java/com/chingluh/android/activity/com/LogoActivity.java
@@ -3,9 +3,11 @@
  */
 package com.chingluh.android.activity.com;
 
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.chingluh.android.R;
 
@@ -17,21 +19,32 @@ public class LogoActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_logo);
-		//线程启动
-		new Thread(new Runnable(){
-			@Override
-			public void run(){
-				//延时
-				try{
-					Thread.sleep(500);//定时1秒
-				}catch(InterruptedException e){
-					e.printStackTrace();
+		//float scale=getResources().getDisplayMetrics().density;
+		try{
+			//
+			//线程启动
+			new Thread(new Runnable(){
+				@Override
+				public void run(){
+					//延时
+					try{
+						Thread.sleep(1000);//定时1秒
+					}catch(InterruptedException e){
+						e.printStackTrace();
+					}
+					//跳转登录页面
+					Intent intentLogin=new Intent(LogoActivity.this,LoginActivity.class);
+					startActivity(intentLogin);
+					finish();
 				}
-				//跳转登录页面
-				Intent intentLogin=new Intent(LogoActivity.this,LoginActivity.class);
-				startActivity(intentLogin);
-				finish();
-			}
-		}).start();
+			}).start();
+			//logo渐变
+			ImageView imageViewLogo=(ImageView)findViewById(R.id.imageViewLogo);
+			ObjectAnimator objectAnimator=ObjectAnimator.ofFloat(imageViewLogo,"alpha",0f,1f);
+			objectAnimator.setDuration(300);
+			objectAnimator.start();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 }
